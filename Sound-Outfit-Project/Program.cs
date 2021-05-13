5387602670c8cef96f215dfd2e1cd7fd217cb9de
Nir <nirjacob33@gmail.com>
2021-05-13 20:53:30 +0300

    Added C# code to support reading sensors and playing sound

---
+++ Sound-Outfit-Project/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.IO.Ports;
+using FireAndForgetAudioSample;
+
+namespace SoundOutfit
+{
+    static class Program
+    {
+        static void Main()
+        {
+            // Arduino print format:
+            //              !<Sensor1_value>@<Sensor2_value>857#<Sensor3_value>$<Sensor4_value>%<Sensor5_value>
+            // example:     !942@0#0$12%932              
+            // I used symbol indicators to save up on bits per loop.                
+            List<Sensor> sensorList = new List<Sensor>();
+            int globalFlexValue = 400;
+            sensorList.Add(new Sensor("closed-hat-trimmed.wav", 1, globalFlexValue));
+            sensorList.Add(new Sensor("crash-trimmed.wav", 2, globalFlexValue));
+            sensorList.Add(new Sensor("kick-trimmed.wav", 3, globalFlexValue));
+            sensorList.Add(new Sensor("open-hat-trimmed.wav", 4, globalFlexValue));
+            sensorList.Add(new Sensor("snare-trimmed.wav", 5, globalFlexValue));
+            int iteration = 0;
+            SerialPort arduinoPort = new SerialPort("COM4",9600);
+            arduinoPort.Open();
+            while (arduinoPort.IsOpen)
+            {
+                string arduinoData = arduinoPort.ReadExisting();
+                string sensorsData = string.Empty;
+                bool numberFound = false;
+                int currentSensor = 0;
+                for (int i = 0; i < arduinoData.Length; i++)
+                {
+                    Sensor.findCurrentSensor(ref currentSensor, arduinoData[i]);
+                    if (Char.IsDigit(arduinoData[i]))
+                    {
+                        sensorsData += arduinoData[i];
+                        numberFound = true;
+                    }
+                    if (numberFound && Char.IsDigit(arduinoData[i]) == false)
+                    {
+                        sensorList[currentSensor].parseValue(sensorsData);
+                        if (sensorList[currentSensor].isFlexed())
+                        {
+                            sensorList[currentSensor].playBeat();
+                        }
+                        sensorsData = string.Empty;
+                        numberFound = false;
+                    }
+                }
+                iteration++;
+                Sensor.printSensorValues(sensorList, iteration % 20);
+                Thread.Sleep(20);
+            }
+            AudioPlaybackEngine.Instance.Dispose();
+        }
+    }
+}
+
---
+++ Sound-Outfit-Project/Sensor.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using FireAndForgetAudioSample;
+
+namespace SoundOutfit
+{
+    class Sensor
+    {
+        CachedSound soundFile;
+        int ID;
+        int currentValue;
+        int flexValue;
+        public Sensor(string filePath, int sensorNum,int flexVal)
+        {
+            soundFile = new CachedSound(filePath);
+            ID = sensorNum;
+            flexValue = flexVal;
+            currentValue = 0;
+        }
+        public int FlexValue
+        {
+            get
+            {
+                return flexValue;
+            }
+            set
+            {
+                flexValue = value;
+            }
+        }
+        public int SensorValue
+        {
+            get
+            {
+                return currentValue;
+            }
+            set
+            {
+                currentValue = value;
+            }
+        }
+        public int SensorNumber
+        {
+            get
+            {
+                return ID;
+            }
+            set
+            {
+                ID = value;
+            }
+        }
+        public void parseValue(string input)
+        {
+            bool parsed = int.TryParse(input, out currentValue);
+            if (parsed==false)
+            {
+                currentValue = 0;
+            }
+        }
+        public bool isFlexed()
+        {
+            if(currentValue > flexValue)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void playBeat()
+        {
+            AudioPlaybackEngine.Instance.PlaySound(soundFile);
+        }
+        public static void findCurrentSensor(ref int index,char ch)
+        {
+            switch (ch)
+            {
+                case '!':
+                    index = 0;
+                    break;
+
+                case '@':
+                    index = 1;
+                    break;
+
+                case '#':
+                    index = 2;
+                    break;
+
+                case '$':
+                    index = 3;
+                    break;
+
+                case '%':
+                    index = 4;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+        public static void printSensorValues(List<Sensor> sensorList,int iteration)
+        {
+            foreach (Sensor sensor in sensorList)
+            {
+                Console.WriteLine("Sensor {0} Value: {1}",sensor.ID,sensor.currentValue);
+            }
+            Console.WriteLine("______________________");
+        }
+
+    }
+}
